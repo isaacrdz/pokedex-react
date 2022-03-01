66b3199e58898bac999bf2a6ceb910be069d1cab
isaacrdz <zack_cpp@hotmail.com>
2022-03-01 10:46:21 -0600

    S

--- src/App.js
+++ src/App.js
@@ -1,159 +1,20 @@
-import React, { useState } from "react";
-import {
-  Container,
-  Grid,
-  TextField,
-  Typography,
-  Button,
-  Box,
-} from "@mui/material";
-import PokemonCard from "./components/PokemonCard";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Pokedex from "./pages/Pokedex";
+import Pokemon from "./pages/Pokemon";
+import Test from "./pages/Test";
 
 const App = () => {
-  const [pokedex, setPokedex] = useState([
-    {
-      name: "Bulbasaur",
-      no: "003",
-      pokemonType: "Grass",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
-    },
-
-    {
-      name: "Charmander",
-      no: "001",
-      pokemonType: "Fire",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
-    },
-
-    {
-      name: "Squirtle",
-      no: "002",
-      pokemonType: "Water",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
-    },
-
-    {
-      name: "Pikachu",
-      no: "020",
-      pokemonType: "Water",
-      image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png",
-    },
-  ]);
-
-  const [pokemon, setPokemon] = useState({
-    name: "",
-    no: "",
-    pokemonType: "",
-    image: "",
-  });
-
-  const { name, no, pokemonType, image } = pokemon;
-
-  const handleOnChange = (e) => {
-    setPokemon({ ...pokemon, [e.target.name]: e.target.value });
-  };
-
-  const clearForm = () => {
-    setPokemon({
-      name: "",
-      no: "",
-      pokemonType: "",
-      image: "",
-    });
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    setPokedex([pokemon, ...pokedex]);
-    clearForm();
-  };
-
-  const handlePokemonCardClick = (pokemon) => {
-    console.log(pokemon);
-  };
-
   return (
-    <>
-      <Grid item xs={12} sx={{ backgroundColor: "#CE3F3A", mb: 3 }}>
-        <Typography sx={{ color: "white", p: 1 }}>Pokedex</Typography>
-      </Grid>
-
-      <Container maxWidth="lg">
-        <Grid container>
-          <Grid item xs={8}>
-            <Grid container spacing={2}>
-              {pokedex.map((pokemon, i) => (
-                <Grid item xs={3} sm={3} md={3} lg={3} key={i}>
-                  <PokemonCard
-                    pokemon={pokemon}
-                    handlePokemonCardClick={handlePokemonCardClick}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </Grid>
-          <Grid item xs={4}>
-            <Box style={{ padding: "1em" }}>
-              <form type="submit">
-                <Box sx={{ mb: 1 }}>
-                  <TextField
-                    label="No."
-                    id="outlined-size-small"
-                    size="small"
-                    fullWidth
-                    name="no"
-                    value={no}
-                    onChange={(e) => handleOnChange(e)}
-                  />
-                </Box>
-                <Box sx={{ mb: 1 }}>
-                  <TextField
-                    label="Name"
-                    id="outlined-size-small"
-                    size="small"
-                    fullWidth
-                    name="name"
-                    value={name}
-                    onChange={(e) => handleOnChange(e)}
-                  />
-                </Box>
-                <Box sx={{ mb: 1 }}>
-                  <TextField
-                    label="Type"
-                    id="outlined-size-small"
-                    size="small"
-                    fullWidth
-                    name="pokemonType"
-                    value={pokemonType}
-                    onChange={(e) => handleOnChange(e)}
-                  />
-                </Box>
-                <Box sx={{ mb: 1 }}>
-                  <TextField
-                    label="Image"
-                    id="outlined-size-small"
-                    size="small"
-                    fullWidth
-                    name="image"
-                    value={image}
-                    onChange={(e) => handleOnChange(e)}
-                  />
-                </Box>
-
-                <Button
-                  variant="outlined"
-                  fullWidth
-                  color="error"
-                  onClick={(e) => handleOnSubmit(e)}
-                >
-                  Add Pokemon
-                </Button>
-              </form>
-            </Box>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+        <Route path="/pokemon/:name" element={<Pokemon />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { Box, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const PokemonCard = ({ pokemon, handlePokemonCardClick }) => {
   const { no, name, pokemonType, image } = pokemon;
   return (
-    <Box
-      onClick={() => handlePokemonCardClick(pokemon)}
-      style={{ display: "flex", justifyContent: "flex-start" }}
-    >
-      <Paper style={{ padding: "1em" }}>
-        <img src={image} style={{ width: "100%", objectFit: "cover" }} />
+    <Link to={`/pokemon/${name}`}>
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          cursor: "pointer",
+        }}
+      >
+        <Paper style={{ padding: "1em" }}>
+          <img src={image} style={{ width: "100%", objectFit: "cover" }} />
 
-        <Box textAlign={"center"}>
-          <Typography>#{no}</Typography>
-          <Typography>{name}</Typography>
-          <Typography>Type:{pokemonType}</Typography>
-        </Box>
-      </Paper>
-    </Box>
+          <Box textAlign={"center"}>
+            <Typography>#{no}</Typography>
+            <Typography>{name}</Typography>
+            <Typography>Type:{pokemonType}</Typography>
+          </Box>
+        </Paper>
+      </Box>
+    </Link>
   );
 };
 
